2bb853ff955580efefc4ed05f142644a3495b81d
xansec <abrewer@forallsecure.com>
2024-08-19 15:34:25 -0400

    use yaml validator for now

--- tests/mayhem_validator.sh
+++ tests/mayhem_validator.sh
@@ -14,7 +14,17 @@ then
     echo "Mayhem CLI installed successfully."
 fi
 
-if mayhem --verbosity debug validate . -f $1; then
+if ! command -v yq &> /dev/null
+then
+    echo "WARNING: yq not found. Downloading manually first."
+    curl --fail -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/bin/yq
+    chmod +x /usr/bin/yq
+    echo "yq installed successfully."
+fi
+
+
+# if mayhem --verbosity debug validate . -f $1; then
+if yq eval $1 > /dev/null; then
   echo "Valid"
   exit 0
 else
